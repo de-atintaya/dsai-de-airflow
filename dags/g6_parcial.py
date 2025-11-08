4fadf1f5271f3902eb7ba166b5aae8eed189a4fe
Italo Flores Sarmiento <ifloressar15@gmail.com>
2025-11-07 22:36:47 -0500

    Parcial

--- dags/g6_parcial.py
+++ dags/g6_parcial.py
@@ -4,9 +4,10 @@ from scripts.azure_upload import upload_to_adls
 from scripts.helpers import add_date_suffix
 from datetime import datetime, timedelta
 
-LOCAL_FILE_PATH = "/opt/airflow/data/sample_g6_ifs.txt"
-CONTAINER_NAME = "datalake" #airflow
-BLOB_NAME = "raw/G6/archivo_subido_g6_ifs.txt"
+LOCAL_FILE_PATH = "/opt/airflow/data/equipos.csv"
+CONTAINER_NAME = "datalake" # airflow
+WASB_CONN_ID = "utec_blob_storage"
+BLOB_NAME = "raw/airflow/G6/equipos.csv"
 
 default_args = {
     'owner': 'airflow',
@@ -15,13 +16,13 @@ default_args = {
 }
 
 @dag(
-    dag_id="g6_ifs",
+    dag_id="g0_utec",
     description="Uploads a local file to Azure Blob Storage with a date suffix.",
     default_args=default_args,
     start_date=datetime(2025, 1, 1, tzinfo=timezone("America/Bogota")),
     schedule="0 12 * * 1", # Runs every monday at 12:00 local time (GMT-5)
     catchup=False,
-    tags=["azure", "blob", "upload"],
+    tags=["utec", "blob", "upload"],
 )
 def upload_dag():
 
@@ -31,7 +32,8 @@ def upload_dag():
         upload_to_adls(
             local_file_path=LOCAL_FILE_PATH,
             container_name=CONTAINER_NAME,
-            blob_name=new_blob_name
+            blob_name=new_blob_name,
+            wasb_conn_id = WASB_CONN_ID
             )
 
     call_upload()
