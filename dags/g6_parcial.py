ebe0219351fbb2bbd41bc61d59bf7af4767bc103
Italo Flores Sarmiento <ifloressar15@gmail.com>
2025-11-22 15:47:58 -0500

    Ajuste archivo csv

--- dags/g6_parcial.py
+++ dags/g6_parcial.py
@@ -32,31 +32,6 @@ default_args = {
 def upload_dag():
 
     @task
-    def prepare_csv_encoding(file_path: str):
-        if not exists(file_path):
-            log.warning(f"File not found: {file_path}. Skipping encoding preparation.")
-            return file_path
-
-        log.info(f"Attempting to read file: {file_path}")
-
-        try:
-            df = pd.read_csv(file_path, encoding='Windows-1252')
-            log.info("Successfully read file using Windows-1252 encoding.")
-        except UnicodeDecodeError:
-            log.warning("Windows-1252 failed. Trying latin1.")
-            try:
-                df = pd.read_csv(file_path, encoding='latin1')
-                log.info("Successfully read file using latin1 encoding.")
-            except Exception as e:
-                log.error(f"Failed to read file with Latin encodings. Error: {e}")
-                raise
-        df.to_csv(file_path, index=False, encoding='utf-8')
-        log.info(f"File successfully re-encoded and saved as UTF-8 at: {file_path}")
-        
-        return file_path # Retorna la ruta del archivo limpio
-
-
-    @task
     def call_upload():
         new_blob_name = add_date_suffix(BLOB_NAME)
         upload_to_adls(
@@ -65,7 +40,6 @@ def upload_dag():
             blob_name=new_blob_name,
             wasb_conn_id = WASB_CONN_ID
             )
-    prepared_path = prepare_csv_encoding(LOCAL_FILE_PATH)
-    call_upload(prepared_path)
+    call_upload()
 
 dag = upload_dag()
