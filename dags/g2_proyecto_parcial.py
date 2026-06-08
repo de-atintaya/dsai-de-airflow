78a058019cb5a0b8d559e250f89bfb06cc7962d7
Jose Carlos Nomberto <jnombert@cisco.com>
2026-06-07 20:17:40 -0500

    Upload DAG to upload file to UTEC BLOB in Azure"
    "

--- dags/g2_proyecto_parcial.py
+++ dags/g2_proyecto_parcial.py
@@ -4,9 +4,10 @@ from scripts.azure_upload import upload_to_adls
 from scripts.helpers import add_date_suffix
 from datetime import datetime, timedelta
 
-LOCAL_FILE_PATH = "/opt/airflow/data/MB51_2023_01.csv"
-CONTAINER_NAME = "airflow"
-BLOB_NAME = "raw/G2/MB51_2023_01_raw.csv"
+LOCAL_FILE_PATH = "/opt/airflow/data/MB51_2023_01_total.csv"
+CONTAINER_NAME = "datalake" # airflow
+WASB_CONN_ID = "utec_blob_storage"
+BLOB_NAME = "raw/airflow/G2/MB51_2023_01_total_raw.csv"
 
 default_args = {
     'owner': 'airflow',
@@ -21,7 +22,7 @@ default_args = {
     start_date=datetime(2025, 1, 1, tzinfo=timezone("America/Bogota")),
     schedule="0 12 * * 1", # Runs every monday at 12:00 local time (GMT-5)
     catchup=False,
-    tags=["azure", "blob", "upload"],
+    tags=["utec", "blob", "upload"],
 )
 def upload_dag():
 
@@ -31,7 +32,8 @@ def upload_dag():
         upload_to_adls(
             local_file_path=LOCAL_FILE_PATH,
             container_name=CONTAINER_NAME,
-            blob_name=new_blob_name
+            blob_name=new_blob_name,
+            wasb_conn_id = WASB_CONN_ID
             )
 
     call_upload()
