5385d5f3407f53020ece41f291e772b960bcd378
Jhomar Yurivilca <jhomaryurivilca@Jhomars-MacBook-Pro.local>
2026-06-09 21:40:35 -0500

    Pipeline transformacion e ingesta

--- scripts/azure_upload.py
+++ scripts/azure_upload.py
@@ -28,3 +28,33 @@ def upload_to_adls(
     except Exception as e:
         log.error("==> Failed to upload to Azure Blob", exc_info=True)
         raise
+
+def upload_folder_to_adls(
+        local_folder_path,
+        container_name = "airflow",
+        blob_prefix = "raw/G1",
+        wasb_conn_id = "azure_blob_storage"
+        ):
+    if not os.path.exists(local_folder_path):
+        log.warning(f"==> Local folder not found: {local_folder_path}")
+        return
+
+    hook = WasbHook(wasb_conn_id=wasb_conn_id)
+    
+    for root, dirs, files in os.walk(local_folder_path):
+        for file in files:
+            local_file_path = os.path.join(root, file)
+            relative_path = os.path.relpath(local_file_path, local_folder_path)
+            blob_name = os.path.join(blob_prefix, relative_path).replace("\\", "/")
+
+            try:
+                log.info(f"==> Uploading {local_file_path} to {container_name}/{blob_name}")
+                hook.load_file(
+                    file_path=local_file_path,
+                    container_name=container_name,
+                    blob_name=blob_name,
+                    overwrite=True
+                )
+            except Exception as e:
+                log.error(f"==> Failed to upload {file} to Azure Blob", exc_info=True)
+                raise
